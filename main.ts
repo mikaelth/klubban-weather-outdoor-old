e6593c2d081313f65040c15e716a4a311456ee89
Mikael Thollesson <mikael.thollesson@ebc.uu.se>
2020-07-20 14:49:22 +0200

    Parametrising

--- main.ts
+++ main.ts
@@ -1,23 +1,46 @@
-/**
- * serial.redirect( SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600 )
- */
+// @param {string} queryParam - The mnemonic for the sensor
+function readWeatherSensor (queryParam: string) {
+    switch(queryParam) {
+        case 'temp':
+            return (weatherbit.temperature() / 100);break;
+        case 'humid':
+            return (weatherbit.humidity() / 1024);break;
+        case 'press':
+            return (weatherbit.pressure() / 25600);break;
+        case 'alt':
+            return weatherbit.altitude();break;
+        case 'wspeed':
+            return weatherbit.windSpeed() / 2.2369362920544;break;
+        case 'wdir':
+        switch (weatherbit.windDirection()) {
+            case 'N': return 0;break;
+            case 'NE': return 45;break;
+            case 'E': return 90;break;
+            case 'SE': return 135;break;
+            case 'S': return 180;break;
+            case 'SW': return 225;break;
+            case 'W': return 270;break;
+            case 'NW': return 315;break;
+            default: return 0; 
+        } break;      
+        case 'rain':
+            return weatherbit.rain() / 25.4; break;
+        default:
+            return 0;
+    }
+}
+let weatherParam = 0
+let mnemonics = ["temp", "humid", "press", "alt", "wspeed", "wdir", "rain"]
+// serial.redirect( SerialPin.P15, SerialPin.P14, BaudRate.BaudRate9600 )
+
 weatherbit.startRainMonitoring()
 weatherbit.startWindMonitoring()
 weatherbit.startWeatherMonitoring()
 serial.redirectToUSB()
+
 basic.forever(function () {
-    serial.writeValue("temperature", weatherbit.temperature() / 100)
-    basic.pause(1000)
-    serial.writeValue("humidity", weatherbit.humidity() / 1024)
-    basic.pause(1000)
-    serial.writeValue("pressure", weatherbit.pressure() / 25600)
-    basic.pause(1000)
-    serial.writeValue("altitude", weatherbit.altitude())
-    basic.pause(1000)
-    serial.writeValue("windspeed", weatherbit.windSpeed() / 2.2369362920544)
-    basic.pause(1000)
-    serial.writeLine("winddir:" + weatherbit.windDirection())
-    basic.pause(1000)
-    serial.writeValue("rain", weatherbit.rain() / 25.4)
-    basic.pause(10000)
+    for (let currentParam of mnemonics) {
+        serial.writeValue(currentParam, readWeatherSensor(currentParam))
+        basic.pause(10000)
+    }
 })
